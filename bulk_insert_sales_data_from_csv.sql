57881dd36097a314dcc6c6896cba14908e9a57cf
asif4499 <36105409+asif4499@users.noreply.github.com>
2025-03-08 02:15:59 +0600

    Update bulk_insert_sales_data_from_csv.sql

--- bulk_insert_sales_data_from_csv.sql
+++ bulk_insert_sales_data_from_csv.sql
@@ -1,6 +1,35 @@
-LOAD DATA INFILE '/path/Superstore_Sales_Data.csv' --replace your file path--
+LOAD DATA INFILE '/var/lib/mysql-files/Superstore_Sales_Data.csv'
 INTO TABLE sales
-FIELDS TERMINATED BY ',' 
+FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(
+    row_id,
+    order_priority,
+    discount,
+    unit_price,
+    shipping_cost,
+    customer_id,
+    customer_name,
+    ship_mode,
+    customer_segment,
+    product_category,
+    product_sub_category,
+    product_container,
+    product_name,
+    @product_base_margin, -- Use a variable to handle empty strings
+    region,
+    manager,
+    state_or_province,
+    city,
+    postal_code,
+    order_date,
+    ship_date,
+    profit,
+    quantity_ordered,
+    sales,
+    order_id,
+    return_status
+)
+SET product_base_margin = IF(@product_base_margin = '', 0, @product_base_margin); -- Replace empty strings with 0
